1eb9ff3b8d60bf955da1639f89b4175fb260f9f7
Synonymous64 <prajwalurkude007@gmail.com>
2023-06-28 21:55:05 +0530

    google config

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,42 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { getProviders, signIn } from "next-auth/react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+};
+
+type Providers = Record<string, Provider>;
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
-}
+  const [Providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      console.log(res);
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (Providers) {
+    return (
+      <div>
+        {Object.values(Providers).map((provider: Provider, i) => (
+          <button key={i} onClick={() => signIn(provider?.id)}>
+            {provider.id}
+          </button>
+        ))}
+      </div>
+    );
+  }
+};
 
 export default AuthProviders;
